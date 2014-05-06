e4685e5c051ca0871dbde946705818f6c9f8488e
charleywu <charleymswu@gmail.com>
2014-05-06 14:30:45 +0200

    actually fixed filepaths'

--- GensimTextProcess.py
+++ GensimTextProcess.py
@@ -3,14 +3,20 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 from gensim import corpora, models, similarities
 from nltk.corpus import stopwords
-import csv
+import csv, os
 
-#Todo List
+#Todo List:
 #Chunk terms by comma separation
 #Intialize Dictionary
 
+#Filepath variable
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+#f = open(os.path.join(__location__, 'bundled-resource.jpg'))
+
 #Create Dictionary from text file
-dictionary = corpora.Dictionary(line.lower().split() for line in open('KeyVisData.txt'))
+dictionary = corpora.Dictionary(line.lower().split() for line in open(os.path.join(__location__,'KeyVisData.txt')))
 
 print dictionary
 
@@ -19,9 +25,9 @@ stop_ids = [dictionary.token2id[stopword] for stopword in stopwords.words('engli
 once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq ==1]
 dictionary.filter_tokens(stop_ids) #remove them from the dictionary "dictionary.filter_tokens(stop_ids + once_ids)"
 dictionary.compactify() # remove gaps in id sequence after words that were removed
-dictionary.save('~/LDA/tmp/KeyVis.dict') #store dictionary for future reference
+dictionary.save(os.path.join(__location__, '/tmp/KeyVis.dict')) #store dictionary for future reference
 
-dictionary = corpora.Dictionary.load('~/tmp/KeyVis.dict')
+dictionary = corpora.Dictionary.load(os.path.join(__location__, '/tmp/KeyVis.dict'))
 print dictionary
 
 
@@ -31,7 +37,7 @@ print dictionary
 #Memory efficient method to read from text without storing in RAM
 class MyCorpus(object):
 	def __iter__(self):
-		for line in open('KeyVisData.csv'):
+		for line in open(os.path.join(__location__,'KeyVisData.txt')):
 			#Assume there's one document per line, tokens separated by whitespace
 			yield dictionary.doc2bow(line.lower().split())
 
@@ -43,10 +49,10 @@ tfidf = models.TfidfModel(corpus) #tfidf is a read-only object that converts any
 corpus_tfidf = tfidf[corpus]
 
 #Save as market matrix
-corpora.MmCorpus.serialize('~tmp/KeyVis_tfidf.mm', corpus_tfidf)
+corpora.MmCorpus.serialize(os.path.join(__location__, 'KeyVis_tfidf.mm'), corpus_tfidf)
 
 
-mm = corpora.MmCorpus('~/tmp/KeyVis_tfidf.mm')
+mm = corpora.MmCorpus(os.path.join(__location__, 'KeyVis_tfidf.mm'))
 print mm
 
 """
