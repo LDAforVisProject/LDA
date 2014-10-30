e6db9fbd18b1b6e044209c26ace68adbe8a42948
Raphael Mitsch <laarem33@hotmail.com>
2014-10-30 14:26:42 +0100

    Added parsing of CLI arguments for k (number of dimensions) and n (number of points).

--- PointDistanceTest/PointDistanceTest.py
+++ PointDistanceTest/PointDistanceTest.py
@@ -8,24 +8,51 @@ from Point import Point
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from optparse import OptionParser
+import logging
 
-# Number of points
-numberOfPoints      = 20
+logger = logging.getLogger(__name__)
+
+# ------------------------------------------------------------------------------------
+# Init option parser
+# ------------------------------------------------------------------------------------
+
+parser = OptionParser()
+parser.add_option("-k", "--k_dimensions", dest="k", type="int", help="write report to FILE", metavar="FILE")
+parser.add_option("-n", "--n_points", dest="n", type="int", help="write report to FILE", metavar="FILE")
+
+# Parse arguments
+#    Determine number of dimensions
+(options, args) = parser.parse_args()
+if options.k == None:
+    logger.info("WARNING: No option given for k (number of dimension. Assuming k = 10).")
+    numberOfDimensions = 50
+else:
+    numberOfDimensions = options.k
+#    Determine number of points     
+if options.k == None:
+    logger.info("WARNING: No option given for n (number of points. Assuming n = 50).")
+    numberOfPoints = 50
+else:
+    numberOfPoints = options.n
+    
 # Number of distances between numberOfPoints points (Gaussian sum formula)
-numberOfDistances   = ( ( (numberOfPoints - 1) * (numberOfPoints - 1) + numberOfPoints - 1) / 2)
-# Number of dimensions
-numberOfDimensions  = 10
+numberOfDistances = ( ( (numberOfPoints - 1) * (numberOfPoints - 1) + numberOfPoints - 1) / 2)
 
 # Coordinate space base ('cube')
 shape = 'cube'
 # Distance function ('L2', 'Bhattacharyya', 'Hellinger', 'KullbackLeibler', 'JensenShannon')
+#distanceFunctionNames = ['L2', 'Bhattacharyya', 'Hellinger', 'KullbackLeibler', 'JensenShannon']
 distanceFunctionName = 'JensenShannon'
+
+# ------------------------------------------------------------------------------------
+# Prepare data, calculate distances
+# ------------------------------------------------------------------------------------
 
 # Data
 points = [Point(dimension=numberOfDimensions, base=shape) for x in range(numberOfPoints)]
 distances = [0] * numberOfDistances 
 
-# Calculate distances
 currentDistanceIndex = 0
 for outerIndex in np.arange(0, numberOfPoints - 1):
     #print outerIndex
@@ -35,6 +62,11 @@ for outerIndex in np.arange(0, numberOfPoints - 1):
         currentDistanceIndex = currentDistanceIndex + 1 
         #print distances[currentDistanceIndex]
 
+
+# ------------------------------------------------------------------------------------
+# Plot data
+# ------------------------------------------------------------------------------------
+
 # Create x-ticks (sqrt(dim) ~ 100%)
 xAxis_translationFactor = 100 / math.sqrt(numberOfDimensions)
 x_ticks_range = range(numberOfDimensions)
@@ -42,7 +74,6 @@ x_ticks = [int((x * xAxis_translationFactor)) for x in x_ticks_range]
 print xAxis_translationFactor
 print x_ticks
 print range(numberOfDimensions)
-
 
 # https://stackoverflow.com/questions/11750276/matplotlib-how-to-convert-a-histogram-to-a-discrete-probability-mass-function
 #distancesNormalized = np.histogram(distances,bins=50,normed=0)[0]/float(len(distances))
